1eb32ba2bf64b759c2bffcb4ebdac3085b530859
Maksim Okunev <max.vic.okunev@gmail.com>
2021-12-13 22:54:36 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,14 @@
+public class Miles {
+    public static void main(String[] args) {
+
+        int tiketPrice = 6535;
+        int oneBonusMileCost = 20;
+        int countBonusMiles = tiketPrice / oneBonusMileCost;
+
+        System.out.println("Количество бонусных миль: " + countBonusMiles);
+
+    }
+}
+
+
+
