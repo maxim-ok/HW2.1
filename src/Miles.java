64f5fe54e37227216afe925aafb2977c4233026c
Maksim Okunev <max.vic.okunev@gmail.com>
2022-02-08 20:29:11 +0300

    Fix changes

--- src/Miles.java
+++ src/Miles.java
@@ -1,11 +1,11 @@
 public class Miles {
     public static void main(String[] args) {
 
-        int tiketPrice = 6535;
+        int tiketPrice = 10000;
         int oneBonusMileCost = 20;
         int countBonusMiles = tiketPrice / oneBonusMileCost;
 
-        System.out.println("Количество бонусных миль: " + countBonusMiles);
+        System.out.println("Количество бонусных миль:  " + countBonusMiles);
 
     }
 }
